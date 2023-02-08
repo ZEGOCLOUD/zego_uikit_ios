adc3a54fd43575e8f80ed99a3cdd7855179ec243
careyxi <careyxi@zego.im>
2023-02-08 19:42:00 +0800

    Fix 2.0.0 xcframework problem.

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
             .product(name: "ZegoPluginAdapter", package: "zego_plugin_adapter_ios"),
             .product(name: "ZegoExpressEngine", package: "express-video-ios")]),
         
-            .binaryTarget(name: "ZegoUIKitSDK", url: "https://github.com/ZEGOCLOUD/zego_uikit_ios/releases/download/2.0.0/ZegoUIKitSDK.xcframework.zip", checksum: "38c625bed0a5d2d6d6e7b86211d33e1a79442ee7f9d426816f9192067e8b5dde"),
+            .binaryTarget(name: "ZegoUIKitSDK", url: "https://github.com/ZEGOCLOUD/zego_uikit_ios/releases/download/2.0.0/ZegoUIKitSDK.xcframework.zip", checksum: "61e3d0806d838f51254b52e3c18b9e3033d4c6e3b75f2dde5e36b39ebd596a37"),
         
     ]
 )
